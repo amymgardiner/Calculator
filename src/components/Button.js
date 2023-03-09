f6825700c34ecc7c99f7738bb7ab10b7cf8014d4
Amy Gardiner <amymgardiner@gmail.com>
2023-03-09 14:27:37 -0600

    finished and tested app

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import classes from './Button.module.css';
 
-function Button({value}) {
+function Button({value, handler}) {
     return (
-        <div className={value == "C" || value == "=" ? `${classes.calcButtons} ${classes.accentButtons}` : `${classes.calcButtons}`}>
+        <div className={value == "C" || value == "=" ? `${classes.calcButtons} ${classes.accentButtons}` : `${classes.calcButtons}`} onClick={()=>handler(value)}>
             {value}
         </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./Home.module.css";
 import Button from './Button';
 
 function Home() {
 
+    const [res, setRes] = useState("")
+
     const buttons = ["C","9","/","8","7","6","*","5","4","3","+","2","1","0","-",".","Del","="];
+
+    const findValue = ()=> {
+        let result = Function("return "+res)();
+        setRes(result.toString());
+    }
+
+    const handler = (arg) => {
+        if(res === "Infinity"){
+            setRes("");
+            return;
+        }
+        if (arg == "C") setRes("");
+        else if(arg == "=") findValue();
+        else if(arg == "Del"){
+            let n = res.length;
+            if (n>0)
+            setRes(res.slice(0,n-1));
+        }
+        else setRes(res.concat(arg));
+    }
+
 
     return (
         <div className={classes.homepage}>
             <div className={classes.calculator}>
                 <div className={classes.display}>
-                    <div className={classes.displayBox}></div>
+                    <div className={classes.displayBox}>
+                        {res}
+                    </div>
                 </div>
                 <div className={classes.buttons}>
-                    {buttons.map((ele,index)=> {return <Button value={ele} key={index}/>})}
+                    {buttons.map((ele,index)=> {return <Button handler={handler} value={ele} key={index}/>})}
                 </div>
             </div>
         </div>
